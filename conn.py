2beb79ef9951870c2ee391cd705af9fb1fa05e75
Uriah Feng <Uriah@Uriah-Feng.local>
2018-09-28 02:14:01 +0800

    My first version

---
+++ conn.py
@@ -0,0 +1,36 @@
+from models import *
+
+hostList = ['10.1.10.128', '10.1.10.127', '10.1.10.120', '10.1.10.119', '10.1.10.122', '10.1.10.125', '10.1.10.130', '10.1.10.83', '10.1.10.173', '10.1.10.174', '10.1.10.175', '10.1.10.176', '10.1.10.177', '10.1.10.178', '10.1.10.179', '10.1.10.180', '10.1.10.181', '10.1.10.182', '10.1.10.183', '10.1.10.184']
+
+for i in hostList:
+    a = Host(i)
+    info = []
+    domainID = []
+    hostname =  a.get_hostname()
+    domainName = a.get_domain()
+    for i in domainName:
+        domainID.append(a.get_domainID(i))
+    for i in range(len(domainName)):
+        domainID[i][0].update({'name': domainName[i]})
+
+#print(domainID)
+#print(domainName)
+
+    for i in range(len(domainID)):
+        if domainID[i][0]['state'] == 'RUNNING':
+            inet = a.retrive_interface(domainName[i])
+            data = {'hostname': hostname, 'id': domainID[i][0]['id'], 'name': domainID[i][0]['name'], 'ip': inet , 'status': domainID[i][0]['state']}
+            info.append(data)
+        else:
+            data = {'hostname': hostname, 'id':  domainID[i][0]['id'], 'name': domainID[i][0]['name'], 'ip': 'None', 'status': domainID[i][0]['state']}
+            info.append(data)
+    
+    a.conn.close()
+    
+    dash = '-' * 150
+    print(dash)
+    print('{:<30} {:<15} {:<45} {:<20} {:<20}'.format('<Hostname>', '<ID>', '<Name>', '<IP>', '<Status>'))
+    for i in range(len(info)):
+        print('{:<30} {:<15} {:<45} {:<20} {:<20}'.format(info[i]['hostname'],info[i]['id'], info[i]['name'], info[i]['ip'], info[i]['status']))
+    
+#print(info)  
---
+++ models.py
@@ -0,0 +1,92 @@
+from __future__ import print_function
+import libvirt
+import sys
+from xml.dom import minidom
+
+        
+class Host:
+    
+    def __init__(self, ip):
+        
+        # Which ip of KVM host you want to acsess.
+        self.ip = ip
+        self.url = 'qemu+ssh://root@' + ip +'/system?socket=/run/libvirt/libvirt-sock'
+        self.conn = libvirt.open(self.url)
+        
+        if self.conn == None:
+            print(f"Failed to open connection to {url}", file=sys.stderr)
+            sys.exit(1)
+        #print('success')
+    def get_hostname(self):
+        name = self.conn.getHostname()
+        return name
+    
+    def get_domain(self):
+        #domains = self.conn.listAllDomains(libvirt.VIR_CONNECT_LIST_DOMAINS_ACTIVE)
+        domains = self.conn.listAllDomains(0)
+        alist = []
+        if len(domains) != 0:
+            for i in domains:
+                aname = i.name()
+                alist.append(aname)
+            return alist
+        else:
+            print('None')
+            sys.exit(1)
+            
+    def get_domainID(self, nameList):        
+        dom = self.conn.lookupByName(nameList)
+        #print(dom.state())
+        domainIDs = []
+        #domainIDs = self.conn.listDomainsID()
+        if dom == None:
+            print('Failed to find the domain '+domName, file=sys.stderr)
+            sys.exit(1)
+
+        id = dom.ID()
+        state, reason = dom.state()
+        
+        if id == -1 and state:
+            if state == libvirt.VIR_DOMAIN_NOSTATE:
+                status = 'NOSTATE'
+            elif state == libvirt.VIR_DOMAIN_RUNNING:
+                status = 'RUNNING'
+            elif state == libvirt.VIR_DOMAIN_BLOCKED:
+                status = 'BLOCKED'
+            elif state == libvirt.VIR_DOMAIN_PAUSED:
+                status = 'PAUSED'
+            elif state == libvirt.VIR_DOMAIN_SHUTDOWN:
+                status = 'SHUTDOWN'
+            elif state == libvirt.VIR_DOMAIN_SHUTOFF:
+                status = 'SHUTOFF'
+            elif state == libvirt.VIR_DOMAIN_CRASHED:
+                status = 'CRASHED'
+            elif state == libvirt.VIR_DOMAIN_PMSUSPENDED:
+                status = 'PMSUSPENDED'
+            else:
+                status = 'UNKNOWN'
+            domainIDs.append(dict({'id': 'None', 'state': status}))
+            #print('The domain is not running so has no ID.')
+        else:
+            domainIDs.append(dict({'id': id, 'state': 'RUNNING'}))
+            #print('The ID of the domain is ' + str(id))
+        #print(domainIDs)
+        return domainIDs
+            
+    def retrive_interface(self, domainName):
+        dom = self.conn.lookupByName(domainName)
+        if dom == None:
+            print('Failed to get the domain object', file=sys.stderr)
+            sys.exit(1)
+        else:
+            print(domainName)
+            ifaces = dom.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_AGENT, 0)
+            for (name, val) in ifaces.items():
+                if val['addrs'] and name != 'lo':
+                    for ipaddr in val['addrs']:
+                        if ipaddr['type'] == libvirt.VIR_IP_ADDR_TYPE_IPV4:
+                            inet = ipaddr['addr']
+                            return inet
+    
+    
+    
